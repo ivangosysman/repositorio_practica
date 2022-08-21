b163633f4764498692ece842210ea7ea8c5d359e
Ivan Andres Gomez <iagranados@sysman.com.co>
2022-08-21 18:43:10 -0500

    Este commit contiene cambios para probar el merge a main

--- backend.py
+++ backend.py
@@ -1,3 +1,4 @@
 # primer archivo de Python creado desde Git Bash 
 # Esta línea es agregada para el segundo commit
 # Esta linea es agregada, recien creada la rama "pruebas"
+# esta nueva linea es para probar el merge hacia la rama "main"
